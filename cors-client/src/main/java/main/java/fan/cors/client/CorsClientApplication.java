5cfd34f4afae88605b51b4ac639adbe07a188e2b
yupao <yupao@2dfire.com>
2019-01-12 18:10:42 +0800

    Test commit

--- cors-client/src/main/java/main/java/fan/cors/client/CorsClientApplication.java
+++ cors-client/src/main/java/main/java/fan/cors/client/CorsClientApplication.java
@@ -3,6 +3,7 @@ package main.java.fan.cors.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CorsClientApplication {
 
